c6784f5eb985c843ce59744434c22000e7e91168
unknown <Daniel@DanielCudney.com>
2019-11-25 12:58:52 -0800

    Adding the main body and side panel to the project Allowing the frame to be set and the development to begin.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
-import Header from "./Components/Header";
 import "./App.css";
+import Header from "./Components/Header";
+import SidePanel from "./Components/SidePanel";
+import MainBody from "./Components/MainBody";
 
 function App() {
   return (
     <div className="App">
       <Header message="Welcome to drag-n-drop" />
+      <SidePanel />
+      <MainBody />
     </div>
   );
 }
