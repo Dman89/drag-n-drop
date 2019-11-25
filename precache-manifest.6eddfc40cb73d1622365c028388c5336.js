0ee390ba5152aea1575645b96688d13a8759464a
Dman89 <Daniel@DanielCudney.com>
2019-11-25 15:37:57 -0800

    Updates

---
+++ precache-manifest.6eddfc40cb73d1622365c028388c5336.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062035c7195c25b1e35f37368a410f24",
+    "url": "/drag-n-drop/index.html"
+  },
+  {
+    "revision": "f338632b40aa4208416d",
+    "url": "/drag-n-drop/static/css/main.877432f5.chunk.css"
+  },
+  {
+    "revision": "ce67a80fb051ed93e3a2",
+    "url": "/drag-n-drop/static/js/2.e1b73cdc.chunk.js"
+  },
+  {
+    "revision": "f338632b40aa4208416d",
+    "url": "/drag-n-drop/static/js/main.78fc019c.chunk.js"
+  },
+  {
+    "revision": "a59f5e22840ec9c99d8b",
+    "url": "/drag-n-drop/static/js/runtime-main.97919b63.js"
+  }
+]);
