0bd48ad00ac75bfbe67121b3105c628958e092c7
unknown <Daniel@DanielCudney.com>
2019-11-25 13:26:52 -0800

    Adding component to test features

---
+++ src/Components/TestComponent/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./index.css";
+
+function TestComponent() {
+  return <div className="Test-Component">Welcome to the Jungle!</div>;
+}
+
+export default TestComponent;
