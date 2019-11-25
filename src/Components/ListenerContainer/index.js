70654ecd16ea4728714500ce73e692dc8b9bfad8
unknown <Daniel@DanielCudney.com>
2019-11-25 13:26:30 -0800

    Adding component to listen for drop event

---
+++ src/Components/ListenerContainer/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./index.css";
+
+function ListenerContainer({ styles }) {
+  return <div className="Listener-Container" style={styles}></div>;
+}
+
+export default ListenerContainer;
