6e8bcdc8ef273296c483ea36017448dafb72b2dc
unknown <Daniel@DanielCudney.com>
2019-11-25 12:45:24 -0800

    Updating header to display a welcome message and center the content to begin to give some form to the content.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Header message="Place Holder" />
+      <Header message="Welcome to drag-n-drop" />
     </div>
   );
 }
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./index.css";
 
 function Header({ message }) {
-  return <header className="App-header">{message}</header>;
+  return (
+    <header className="App-Header">
+      <div className="Header-Content">{message}</div>
+    </header>
+  );
 }
 
 export default Header;
